79483867910782ad262cf1a75a2d4040257b5c19
Fernández Zavaleta <fz21i04001@usonsonate.edu.sv>
2024-09-21 10:29:18 -0600

    Conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 //inicializando
 const app = express();
 
+require('dotenv').config();
 //ajustes del servidor
-app.set('port', process.env.PORT || 4000);
+
+app.set("port", process.env.PORT || 4500)
+
+app.use(require('./routes'))
+
 
 //inicializando el servidor
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log(`Servidor iniciado en el puerto ${app.get('port')}`);
 })
